a6898bd632ca2af7fcd53b1a99ca6da7f209e07f
Paweł <pawel1116574@gmail.com>
2021-05-17 18:02:09 +0200

    dodano rozkład iteracyjny

--- przyklady-algorytmow/algorytm_euklidesa.py
+++ przyklady-algorytmow/algorytm_euklidesa.py
@@ -1,12 +1,12 @@
 # def  NWD(a, b):  -- to też by działało
-def  NWD_rekursywne(a: int, b: int) -> int:
+def  NWD_rekurencyjnie(a: int, b: int) -> int:
     remainder = a % b
     if remainder == 0:
         return b
     else:
         a = b
         b = remainder
-        return NWD_rekursywne(a, b)
+        return NWD_rekurencyjnie(a, b)
 
 
 def NWD_iteracyjnie(a: int, b: int) -> int:
@@ -19,13 +19,30 @@ def NWD_iteracyjnie(a: int, b: int) -> int:
     return b
 
 
-def czynniki_rekursywnie():
+def czynniki_rekurencyjnie(n, k=2):
     return NotImplementedError
+    if n % k == 0:
+        yield k
+        yield from czynniki_rekurencyjnie(n//k, k)
+    else:
+        yield from czynniki_rekurencyjnie(n//k, k+1)
 
-def czynniki_iteracyjnie():
-    return NotImplementedError
+def czynniki_iteracyjnie(n):
+    # src = https://maturka.it/algorytmy/python-rozklad-liczby-na-czynniki-pierwsze/
+    czynniki = []
+    k = 2
+    while n != 1:
+        while n % k == 0:
+            n //= k
+            czynniki.append(k)
+        k += 1
+    
+    return czynniki
 
 if __name__ == '__main__':
-    # print(NWD_rekursywne(282, 78))
+    print(NWD_rekurencyjnie(282, 78))
     print(NWD_iteracyjnie(282, 78))
+    print(czynniki_iteracyjnie(212))
+    print(list(czynniki_rekurencyjnie(6)))
+
 
